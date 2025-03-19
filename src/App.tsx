b51b6c229e243cf6771a1c0736c27b61b48348bc
JulienS-Code <julien.synaeve@gmail.com>
2025-03-19 11:56:08 +0100

    Added react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
+import AppRoutes from "./route";
+
 function App() {
-    return (
-        <div className="flex min-h-screen items-center justify-center bg-background">
-            <h1 className="text-4xl font-bold text-primary hover:text-primary/50 transition-colors duration-300">
-                Hello World - Testing Tailwind CSS
-            </h1>
-        </div>
-    )
+    return <AppRoutes />;
 }
 
-export default App
+export default App;
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,11 @@
+function Home() {
+    return (
+        <div className="flex min-h-screen items-center justify-center bg-background">
+            <h1 className="text-4xl font-bold text-primary hover:text-primary/50 transition-colors duration-300">
+                Hello World - Testing Tailwind CSS
+            </h1>
+        </div>
+    )
+}
+
+export default Home
