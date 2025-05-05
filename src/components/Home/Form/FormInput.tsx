3aee0abec932e012ef8548de206384e8061175ca
synaeve <synaeve@idmgroup.com>
2025-05-05 23:09:11 +0200

    TRA-28: Gestion de login - Background image optimisation (using webp) - Updated focus color input

--- src/components/Home/Form/FormInput.tsx
+++ src/components/Home/Form/FormInput.tsx
@@ -32,8 +32,8 @@ export const FormInput: React.FC<FormInputProps> = ({
 
     const selectSuggestion = (s: string) => {
         if (onChange) {
-            // Simule un event pour la valeur
-            onChange({currentTarget: {value: s}} as any);
+            const event = {target: { value: s },} as React.ChangeEvent<HTMLInputElement>;
+            onChange(event);
         }
         setOpen(false);
     };
@@ -43,13 +43,13 @@ export const FormInput: React.FC<FormInputProps> = ({
     return (
         <div className="flex flex-col grow shrink min-w-60 w-[323px]">
             <label className="gap-2.5 self-start text-sm font-medium text-center text-gray-700">{label}</label>
-            <div className="flex relative justify-between items-start mt-2 w-full min-h-[55px]">
+            <div className="flex relative justify-between items-start mt-2 w-full min-h-[55px] rounded-lg focus-within:ring-2 focus-within:ring-primary">
                 <input
                     type={type}
                     placeholder={placeholder}
                     required={required}
                     {...inputProps}
-                    className={`z-0 flex-1 shrink self-stretch my-auto text-base leading-6 placeholder:text-gray-400 text-textPrimary bg-white rounded-lg border border-gray-300 border-solid basis-0 min-w-60 w-[231px] min-h-[55px] ${icon ? "pl-12" : "pl-4"}`}
+                    className={`z-0 flex-1 shrink self-stretch my-auto text-base leading-6 placeholder:text-gray-400 text-textPrimary bg-white rounded-lg border border-gray-300 border-solid basis-0 min-w-60 w-[231px] min-h-[55px] ${icon ? "pl-12" : "pl-4"} focus:outline-none`}
                 />
                 {icon && (
                     <div className="flex overflow-hidden absolute left-5 z-0 justify-center items-center self-start w-3.5 h-4 bottom-[20px] min-h-4">
